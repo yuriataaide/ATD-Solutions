b87094a9238f3fad4e72f952e111cba3a47bb126
Gabi Sartori <yuriizinho@outlook.com>
2026-05-12 13:27:51 -0300

    change testimonials interval

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -46,7 +46,7 @@ export function Testimonials() {
     fetchTestimonials()
     
     // Polling para atualização em tempo real (a cada 10 segundos)
-    const interval = setInterval(fetchTestimonials, 60000)
+    const interval = setInterval(fetchTestimonials, 6000000)
     return () => clearInterval(interval)
   }, [fetchTestimonials])
 
